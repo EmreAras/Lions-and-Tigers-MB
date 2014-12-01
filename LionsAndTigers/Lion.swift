82951d7112708203aff9936a09b154c38ddae3b6
Emre <emrearas07@gmail.com>
2014-12-01 10:59:36 +0000

    New Class
    Classes and Constructs

---
+++ LionsAndTigers/Lion.swift
@@ -0,0 +1,21 @@
+//
+//  Lion.swift
+//  LionsAndTigers
+//
+//  Created by Emre on 01/12/2014.
+//  Copyright (c) 2014 CTS . All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class Lion {
+    
+    var age = 0
+    var isAlphaMale = false
+    var image = UIImage(named: "")
+    var name = ""
+    var subspecies = ""
+    
+}
